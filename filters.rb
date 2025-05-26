1ca14e991ea775b7820d10ab438e79476bb1a1ae
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-25 21:36:52 -0700

    completed assignment as per instructions

--- filters.rb
+++ filters.rb
@@ -2,15 +2,39 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+  for candidate in candidates
+    return candidate if candidate[:id] == id
   end
+  nil
+end
   
-  def experienced?(candidate)
-    # Your code Here
+def experienced?(candidate)
+  if candidate[:years_of_experience] > 2
+    true
+  else
+    false
   end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
+end
+
+def qualified_candidates(candidates)
+  arr_of_qualified_candidates = []
+
+  for candidate in candidates
+    if experienced?(candidate) && 
+      candidate[:github_points] >= 100 && 
+      candidate[:age] > 17 && 
+      candidate[:date_applied] >= 15.days.ago.to_date && 
+      (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python'))
+
+      # push candidates into an array
+      arr_of_qualified_candidates << candidate
+    end
   end
-  
-  # More methods will go below
+
+  arr_of_qualified_candidates
+end
+
+def ordered_by_qualifications(candidates)
+  sorted_by_experience = candidates.sort_by { |candidate| [candidate[:years_of_experience], candidate[:github_points]]}
+  reversed_sorted_by_experience = sorted_by_experience.reverse
+end
